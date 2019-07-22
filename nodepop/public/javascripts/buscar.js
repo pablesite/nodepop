9cbb05085d356e84d9e9ee126b3b185c4684866d
Pablo Ruiz <pabloruiz@ctnaval.com>
2019-07-23 01:27:59 +0200

    Arreglo una incidencia que tenía la función buscar

--- nodepop/public/javascripts/buscar.js
+++ nodepop/public/javascripts/buscar.js
@@ -3,7 +3,7 @@
 function buscar()
 {
     let textBuscar = document.getElementById("textBuscar").value;
-    window.location = '/apiv1/anuncios?nombre=' + textBuscar;
+    window.location = '/anuncios?nombre=' + textBuscar;
    
 }
     
